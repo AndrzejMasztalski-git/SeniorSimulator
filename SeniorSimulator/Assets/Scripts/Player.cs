3e371ea9b12e2fea9a3c5d8ff40de0998d1730c3
andrzejmasztalski <72304329+AndrzejMasztalski-git@users.noreply.github.com>
2023-04-21 12:03:23 +0200

    Wybór postaci i przechodzenie z wyboru postaci do gry

--- SeniorSimulator/Assets/Scripts/Player.cs
+++ SeniorSimulator/Assets/Scripts/Player.cs
@@ -1,14 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    public CharacterDatabase characterDB;
+    public SpriteRenderer artworkSprite;
+
+    private int selectedOption = 0;
+
+
     public int maxHealth = 100;
     public int currentHealth;
     public HealthBar healthBar;
     void Start()
     {
+        if (!PlayerPrefs.HasKey("selectedOption"))
+        {
+            selectedOption = 0;
+        }
+        else
+        {
+            Load();
+        }
+
+        UpdateCharacter(selectedOption);
+
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
     }
@@ -26,5 +44,19 @@ public class Player : MonoBehaviour
     {
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
+    }
+
+
+    private void UpdateCharacter(int selectedOption)
+    {
+        Character character = characterDB.GetCharacter(selectedOption);
+
+        artworkSprite.sprite = character.characterSprite;
+        
+    }
+
+    private void Load()
+    {
+        selectedOption = PlayerPrefs.GetInt("selectedOption");
     }
 }

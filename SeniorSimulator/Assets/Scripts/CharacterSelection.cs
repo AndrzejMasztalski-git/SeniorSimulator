0b5e4c2dc72c62dc2d30fdf963d9196d37e0cf47
andrzejmasztalski <72304329+AndrzejMasztalski-git@users.noreply.github.com>
2023-04-23 11:32:55 +0200

    3D menu wyboru + dostosowanie grywalnego modelu do wyboru w menu

---
+++ SeniorSimulator/Assets/Scripts/CharacterSelection.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelection : MonoBehaviour
+{
+    private GameObject[] characterList;
+    private int index;
+    private void Start()
+    {
+        index = PlayerPrefs.GetInt("CharacterSelected");
+
+        characterList = new GameObject[2];
+        // Wype³nianie tablicy modelami
+        for (int i = 0; i < transform.childCount; i++)
+            characterList[i] = transform.GetChild(i).gameObject;
+
+        // wy³¹czanie renderera modeli
+        foreach (GameObject go in characterList)
+            go.SetActive(false);
+        
+        // W³¹czenie wybranego modelu
+        if (characterList[index])
+            characterList[index].SetActive(true);
+        
+    }
+
+    public void ToggleLeft()
+    {
+        // Wy³¹cz aktualny model
+        characterList[index].SetActive(false);
+
+
+        index--;
+        if(index < 0)
+        {
+            index = characterList.Length - 1;
+        }
+
+
+        // W³¹cz nowy model
+        characterList[index].SetActive(true);
+    }
+
+    public void ToggleRight()
+    {
+        // Wy³¹cz aktualny model
+        characterList[index].SetActive(false);
+
+
+        index++;
+        if (index == characterList.Length)
+        {
+            index = 0;
+        }
+
+
+        // W³¹cz nowy model
+        characterList[index].SetActive(true);
+    }
+
+    public void ChangeScene()
+    {
+        PlayerPrefs.SetInt("CharacterSelected", index);
+        SceneManager.LoadScene(1);
+    }
+}
--- SeniorSimulator/Assets/Scripts/Player.cs
+++ SeniorSimulator/Assets/Scripts/Player.cs
@@ -5,10 +5,9 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    public CharacterDatabase characterDB;
-    //public SpriteRenderer artworkSprite;
+    
 
-    private int selectedOption = 0;
+    
 
 
     public int maxHealth = 100;
@@ -16,16 +15,7 @@ public class Player : MonoBehaviour
     public HealthBar healthBar;
     void Start()
     {
-        if (!PlayerPrefs.HasKey("selectedOption"))
-        {
-            selectedOption = 0;
-        }
-        else
-        {
-            Load();
-        }
-
-        //UpdateCharacter(selectedOption);
+        
 
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
@@ -44,19 +34,5 @@ public class Player : MonoBehaviour
     {
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
-    }
-
-
-    //private void UpdateCharacter(int selectedOption)
-    //{
-    //    Character character = characterDB.GetCharacter(selectedOption);
-
-    //    artworkSprite.sprite = character.characterSprite;
-        
-    //}
-
-    private void Load()
-    {
-        selectedOption = PlayerPrefs.GetInt("selectedOption");
     }
 }

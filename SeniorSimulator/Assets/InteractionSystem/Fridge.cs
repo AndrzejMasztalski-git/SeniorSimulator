d3fcec091d484a35245fd8150eeb2fe021282f89
Jakub <kuba.skoczylas30@gmail.com>
2023-05-18 20:08:42 +0200

    poprawka poprawki

--- SeniorSimulator/Assets/InteractionSystem/Fridge.cs
+++ SeniorSimulator/Assets/InteractionSystem/Fridge.cs
@@ -23,7 +23,7 @@ public class Fridge : MonoBehaviour, IInteractable
         option2Text.text = "Eat something";
         option3Text.text = "Drink beer";
         option1.onClick.AddListener(() => { Debug.Log("Drinking water!"); panel.SetActive(false); player.Heal(10); });
-        option2.onClick.AddListener(() => { Debug.Log("Eating something"); panel.SetActive(false); });
+        option2.onClick.AddListener(() => { Debug.Log("Eating something"); panel.SetActive(false); player.Heal(20); });
         option3.onClick.AddListener(() => { Debug.Log("Drinking beer"); panel.SetActive(false); player.TakeDamage(10); });
         return true;
     }

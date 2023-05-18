d939ea5a9e0dc308e4b3b1df5b6e52ec26ff41a8
Miloszkuropka <72351115+Miloszkuropka@users.noreply.github.com>
2023-05-18 13:27:22 +0200

    camera improvement

--- SeniorSimulator/Assets/InteractionSystem/Fridge.cs
+++ SeniorSimulator/Assets/InteractionSystem/Fridge.cs
@@ -24,7 +24,7 @@ public class Fridge : MonoBehaviour, IInteractable
         option3Text.text = "Drink beer";
         option1.onClick.AddListener(() => { Debug.Log("Drinking water!"); panel.SetActive(false); player.Heal(10); });
         option2.onClick.AddListener(() => { Debug.Log("Eating something"); panel.SetActive(false); });
-        option3.onClick.AddListener(() => { Debug.Log("Drinking beer"); panel.SetActive(false); });
+        option3.onClick.AddListener(() => { Debug.Log("Drinking beer"); panel.SetActive(false); player.TakeDamage(10); });
         return true;
     }
 }

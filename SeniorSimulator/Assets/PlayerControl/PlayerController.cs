9ef5f9a2f53a5a92700138a53130c8f2347a64ef
andrzejmasztalski <72304329+AndrzejMasztalski-git@users.noreply.github.com>
2023-06-03 13:00:33 +0200

    Player controller changed from the bottom, animations work, problem with character height

---
+++ SeniorSimulator/Assets/PlayerControl/PlayerController.cs
@@ -0,0 +1,64 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 4;
+    private PlayerInputsManager input;
+    private CharacterController controller;
+    private Animator animator;
+
+    [SerializeField] GameObject mainCam;
+    [SerializeField] Transform cameraFollowTarget;
+    float xRotation;
+    float yRotation;
+
+    [Range(0f, 10f)] public float mouseSensitivity = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        input = GetComponent<PlayerInputsManager>();
+        controller = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = 0;
+        Vector3 inputDir = new Vector3(input.move.x, 0, input.move.y);
+        float targetRotation = 0;
+       if(input.move != Vector2.zero)
+        {
+            speed = moveSpeed;
+            targetRotation = Quaternion.LookRotation(inputDir).eulerAngles.y + mainCam.transform.rotation.eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0, targetRotation, 0);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 20 * Time.deltaTime);
+        }
+        animator.SetFloat("speed", input.move.magnitude);
+        Vector3 targetDirection = Quaternion.Euler(0,targetRotation,0) * Vector3.forward;
+        controller.Move(targetDirection * speed * Time.deltaTime);
+    }
+
+    private void LateUpdate()
+    {
+        CameraRotation();
+    }
+
+    void CameraRotation()
+    {
+        xRotation += input.look.y;
+        yRotation += input.look.x;
+        xRotation = Mathf.Clamp(xRotation, -30, 70);
+
+        Quaternion rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        cameraFollowTarget.rotation = rotation;
+    }
+}

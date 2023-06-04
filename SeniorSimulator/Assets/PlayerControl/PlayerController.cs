5c561269e78a7f491d5dbfed83cc577e5bcd4e6b
Jakub <kuba.skoczylas30@gmail.com>
2023-06-04 02:48:54 +0200

    dodana babcia, zmiany z dziadkiem
    eustachy wymaga poprawy skórki bo cos sie zepsulo, dodatkowo potrzebna będzie pomoc przy edycji jego kontrolera dla ruchu w biegu.

--- SeniorSimulator/Assets/PlayerControl/PlayerController.cs
+++ SeniorSimulator/Assets/PlayerControl/PlayerController.cs
@@ -11,7 +11,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float moveSpeed = 4;
     private PlayerInputsManager input;
     private CharacterController controller;
-    private Animator animator;
+    Animator animator;
 
     [SerializeField] GameObject mainCam;
     [SerializeField] Transform cameraFollowTarget;
@@ -20,13 +20,23 @@ public class PlayerController : MonoBehaviour
 
     [Range(0f, 10f)] public float mouseSensitivity = 1f;
 
+
+    public float walkSpeed = 2;
+    public float runSpeed = 6;
+    public float turnSmootTime = 0.2f;
+    float turnSmoothVelocity;
+    public float speedSmoothTime = 0.1f;
+    float speedSmoothVelocity;
+    float currentSpeed;
+
+
     // Start is called before the first frame update
     void Start()
     {
-       
+
         input = GetComponent<PlayerInputsManager>();
         controller = GetComponent<CharacterController>();
-        animator = GetComponentInChildren<Animator>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -35,16 +45,38 @@ public class PlayerController : MonoBehaviour
         float speed = 0;
         Vector3 inputDir = new Vector3(input.move.x, 0, input.move.y);
         float targetRotation = 0;
-       if(input.move != Vector2.zero)
+        if (input.move != Vector2.zero)
         {
             speed = moveSpeed;
             targetRotation = Quaternion.LookRotation(inputDir).eulerAngles.y + mainCam.transform.rotation.eulerAngles.y;
             Quaternion rotation = Quaternion.Euler(0, targetRotation, 0);
             transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 20 * Time.deltaTime);
         }
-        animator.SetFloat("speed", input.move.magnitude);
-        Vector3 targetDirection = Quaternion.Euler(0,targetRotation,0) * Vector3.forward;
+        animator.SetFloat("speedPercent", input.move.magnitude);
+        Vector3 targetDirection = Quaternion.Euler(0, targetRotation, 0) * Vector3.forward;
         controller.Move(targetDirection * speed * Time.deltaTime);
+
+
+        // Poruszanie siê, walk i run, smooth animacje i obrót, ale nie dziala z reszt¹, Andrzej bedziesz podtrzebny:
+
+        //Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        //Vector2 inputDir = input.normalized;
+
+        //if (inputDir != Vector2.zero)
+        //{
+        //    float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg;
+        //    transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmootTime);
+        //}
+
+        //bool running = Input.GetKey(KeyCode.LeftShift);
+        //float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
+        //currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+        //transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
+
+        //float animationSpeedPercent = ((running) ? 1 : .5f) * inputDir.magnitude;
+
+        //animator.SetFloat("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+
     }
 
     private void LateUpdate()
@@ -54,8 +86,8 @@ public class PlayerController : MonoBehaviour
 
     void CameraRotation()
     {
-        xRotation += input.look.y/4;
-        yRotation += input.look.x/4;
+        xRotation += input.look.y / 4;
+        yRotation += input.look.x / 4;
         xRotation = Mathf.Clamp(xRotation, -70, 30);
 
         Quaternion rotation = Quaternion.Euler(-xRotation, yRotation, 0);
